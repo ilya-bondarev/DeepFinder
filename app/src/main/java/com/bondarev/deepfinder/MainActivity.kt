3bb3c64750d191484cf71c4365e3eed67263b353
Ilya Bondarev <my.own.securyti@protonmail.com>
2022-10-21 14:02:05 +0300

    Vk_auth

--- app/src/main/java/com/bondarev/deepfinder/MainActivity.kt
+++ app/src/main/java/com/bondarev/deepfinder/MainActivity.kt
@@ -2,10 +2,21 @@ package com.bondarev.deepfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        authLauncher.launch(arrayListOf(VKScope.WALL))
         setContentView(R.layout.activity_main)
+    }
+    private val authLauncher = VK.login(this) { result: VKAuthenticationResult ->
+        when (result) {
+            is VKAuthenticationResult.Success -> Toast.makeText(this, "Авторизация прошла успешно", Toast.LENGTH_LONG).show()
+            is VKAuthenticationResult.Failed -> Toast.makeText(this, "Ошибка авторизации", Toast.LENGTH_LONG).show()
+        }
     }
 }
