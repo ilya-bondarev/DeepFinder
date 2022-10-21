0acd37f909a8ef62354de0386da978620337f291
Ilya Bondarev <my.own.securyti@protonmail.com>
2022-10-21 12:56:01 +0300

    Initial commit

---
+++ app/src/main/java/com/bondarev/deepfinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bondarev.deepfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
